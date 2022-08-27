477e00e36562915077496af50b57b872ecdfe70d
Hijazi <mohammedhijazi199@gmail.com>
2022-08-27 22:59:57 +0300

    make logout route

--- resources/views/messenger.blade.php
+++ resources/views/messenger.blade.php
@@ -2419,9 +2419,15 @@
                         <a href="#tab-settings" class="text-reset" data-theme-toggle="tab" title="Settings" data-bs-dismiss="modal">Settings</a>
                     </li>
 
-                    <li class="list-group-item">
-                        <a href="#" class="text-danger">Logout</a>
-                    </li>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-responsive-nav-link :href="route('logout')"
+                                               onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-responsive-nav-link>
+                    </form>
                 </ul>
                 <!-- List -->
             </div>
